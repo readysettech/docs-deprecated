393e589ecf1d56d88fe5391fe295cfac6c1b80ee
Alana Marzoev <alanamarzoev@gmail.com>
2021-10-27 21:25:44 -0400

    halfway thru the concept docs

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,8 @@ import 'nextra-theme-docs/style.css'
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
+import Prism from 'prism-react-renderer/prism'
+(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-sql")
---
+++ pages/imgs/index.js
@@ -0,0 +1,1 @@
+<img src="imgs/readyset_arch.png" />
