865a8288df4b07591a54f26782c6ef99401b490d
Alana Marzoev <alanamarzoev@gmail.com>
2021-10-29 06:25:47 -0400

    remove nextra references

--- theme.config.js
+++ theme.config.js
@@ -16,7 +16,6 @@ export default {
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
