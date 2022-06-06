608fbd42ad25892e4d2e6c025b1be7be9583bc80
jbensal1 <jay.bensal@gmail.com>
2022-06-06 10:26:58 -0700

    new structure of public documenation site
    This commit adds in a new structure and content ahead
    of opening up the installer to the ReadySet waitlist.

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-
 import Prism from 'prism-react-renderer/prism'
 (typeof global !== "undefined" ? global : window).Prism = Prism
 // require("prismjs/components/prism-sql")
