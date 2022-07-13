dd3568ba175d5601bf3bcb7c5c2d902d7ad9ba7b
jbensal1 <jay.bensal@gmail.com>
2022-07-12 17:00:55 -0700

    Add query support documentation
    This commit adds in a list of supported query
    types and other instructions for querying ReadySet.

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
+  strictMode: true
 })
 module.exports = withNextra()
